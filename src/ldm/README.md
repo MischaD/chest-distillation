a003d8c5b281f97acbb2c56f7b3c318a2f217526
mischa <mischa.dombrowski@fau.de>
2023-01-10 10:00:14 +0100

    Clip installation path changed

---
+++ src/ldm/README.md
@@ -0,0 +1,1 @@
+Code takeen from https://github.com/Stability-AI/stablediffusion
--- src/ldm/modules/encoders/modules.py
+++ src/ldm/modules/encoders/modules.py
@@ -144,7 +144,7 @@ class FrozenOpenCLIPEmbedder(AbstractEncoder):
                  freeze=True, layer="last"):
         super().__init__()
         assert layer in self.LAYERS
-        model, _, _ = open_clip.create_model_and_transforms(arch, device=torch.device('cpu'), pretrained=version)
+        model, _, _ = open_clip.create_model_and_transforms(arch, device=torch.device('cpu'), pretrained=version, cache_dir="./clip/")
         del model.visual
         self.model = model
 
@@ -192,22 +192,5 @@ class FrozenOpenCLIPEmbedder(AbstractEncoder):
     def encode(self, text):
         return self(text)
 
-
-class FrozenCLIPT5Encoder(AbstractEncoder):
-    def __init__(self, clip_version="openai/clip-vit-large-patch14", t5_version="google/t5-v1_1-xl", device="cuda",
-                 clip_max_length=77, t5_max_length=77):
-        super().__init__()
-        self.clip_encoder = FrozenCLIPEmbedder(clip_version, device, max_length=clip_max_length)
-        self.t5_encoder = FrozenT5Embedder(t5_version, device, max_length=t5_max_length)
-        print(f"{self.clip_encoder.__class__.__name__} has {count_params(self.clip_encoder)*1.e-6:.2f} M parameters, "
-              f"{self.t5_encoder.__class__.__name__} comes with {count_params(self.t5_encoder)*1.e-6:.2f} M params.")
-
-    def encode(self, text):
-        return self(text)
-
-    def forward(self, text):
-        clip_z = self.clip_encoder.encode(text)
-        t5_z = self.t5_encoder.encode(text)
-        return [clip_z, t5_z]
 
 
