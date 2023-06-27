204cda1a7faecbdf8838f99ad832e34b26267692
mischa <mischa.dombrowski@fau.de>
2023-06-27 08:00:55 +0200

    Evalution refactoring

--- src/evaluation/utils.py
+++ src/evaluation/utils.py
@@ -1,6 +1,10 @@
 from sklearn.metrics import jaccard_score
 import numpy as np
 import torch
+from log import logger
+from einops import reduce, rearrange
+import os
+import cv2
 
 
 def compute_prediction_from_binary_mask(binary_prediction):
@@ -33,3 +37,144 @@ def compute_metrics(x, y, h, w, binary_prediction):
                       )
     return iou, miou, distance, bbox_pred
 
+
+def get_latent_slice(batch, opt):
+    ds_slice = []
+    for slice_ in batch["slice"]:
+        if slice_.start is None:
+            ds_slice.append(slice(None, None, None))
+        else:
+            ds_slice.append(slice(slice_.start // opt.f, slice_.stop // opt.f, None))
+    return tuple(ds_slice)
+
+def apply_rect(img, x, y, h, w, color="red"):
+    img = (img * 255).to(torch.uint8).numpy()
+    img = rearrange(img, "c h w -> h w c")
+    if color == "red":
+        color = (255, 0, 0)
+    elif color == "blue":
+        color = (0, 0, 255)
+
+    img = cv2.rectangle(img.copy(), [x, y], [x + h, y + w], color, 3)
+    img = rearrange(img, "h w c -> c h w") / 255.
+    return torch.tensor(img)
+
+
+def add_viz_of_data_and_pred(images, batch, x_samples_ddim, opt):
+    # append input
+    x0_norm = torch.clamp((batch["x"] + 1.0) / 2.0, min=0.0, max=1.0).cpu()
+    x0_norm = reduce(x0_norm, 'b c (h h2) (w w2) -> b c h w', 'mean', h2=opt.f, w2=opt.f)
+    images.append(x0_norm)
+
+    # append model output
+
+    x_samples_ddim = torch.clamp((x_samples_ddim + 1.0) / 2.0, min=0.0, max=1.0).cpu()
+    images.append(
+        reduce(x_samples_ddim, 'b c (h h2) (w w2) -> b c h w', 'mean', h2=opt.f, w2=opt.f))
+
+    # append gt mask
+    images.append(
+        reduce(batch["segmentation_x"], 'b c (h h2) (w w2) -> b c h w', 'max', h2=opt.f, w2=opt.f))
+
+
+def contrast_to_noise_ratio(ground_truth_img, prelim_mask_large):
+    gt_mask = ground_truth_img.flatten()
+    pr_mask = prelim_mask_large.flatten()
+
+    roi_values = pr_mask[gt_mask == 1.0]
+    not_roi_values = pr_mask[gt_mask != 1.0]
+
+    contrast = roi_values.mean() - not_roi_values.mean()
+    noise = torch.sqrt(
+        roi_values.var() + not_roi_values.var()
+    )
+    cnr = contrast / noise
+    return cnr
+
+
+def check_mask_exists(mask_dir, samples):
+    for i in range(len(samples["rel_path"])):
+        path = os.path.join(mask_dir, samples["rel_path"][i] + ".pt")
+        if not os.path.exists(path):
+            return False
+    return True
+
+
+
+def get_latent_slice(batch, opt):
+    ds_slice = []
+    for slice_ in batch["slice"]:
+        if slice_.start is None:
+            ds_slice.append(slice(None, None, None))
+        else:
+            ds_slice.append(slice(slice_.start // opt.f, slice_.stop // opt.f, None))
+    return tuple(ds_slice)
+
+def apply_rect(img, x, y, h, w, color="red"):
+    img = (img * 255).to(torch.uint8).numpy()
+    img = rearrange(img, "c h w -> h w c")
+    if color == "red":
+        color = (255, 0, 0)
+    elif color == "blue":
+        color = (0, 0, 255)
+
+    img = cv2.rectangle(img.copy(), [x, y], [x + h, y + w], color, 3)
+    img = rearrange(img, "h w c -> c h w") / 255.
+    return torch.tensor(img)
+
+
+def add_viz_of_data_and_pred(images, batch, x_samples_ddim, opt):
+    # append input
+    x0_norm = torch.clamp((batch["x"] + 1.0) / 2.0, min=0.0, max=1.0).cpu()
+    x0_norm = reduce(x0_norm, 'b c (h h2) (w w2) -> b c h w', 'mean', h2=opt.f, w2=opt.f)
+    images.append(x0_norm)
+
+    # append model output
+
+    x_samples_ddim = torch.clamp((x_samples_ddim + 1.0) / 2.0, min=0.0, max=1.0).cpu()
+    images.append(
+        reduce(x_samples_ddim, 'b c (h h2) (w w2) -> b c h w', 'mean', h2=opt.f, w2=opt.f))
+
+    # append gt mask
+    images.append(
+        reduce(batch["segmentation_x"], 'b c (h h2) (w w2) -> b c h w', 'max', h2=opt.f, w2=opt.f))
+
+
+def contrast_to_noise_ratio(ground_truth_img, prelim_mask_large):
+    gt_mask = ground_truth_img.flatten()
+    pr_mask = prelim_mask_large.flatten()
+
+    roi_values = pr_mask[gt_mask == 1.0]
+    not_roi_values = pr_mask[gt_mask != 1.0]
+
+    contrast = roi_values.mean() - not_roi_values.mean()
+    noise = torch.sqrt(
+        roi_values.var() + not_roi_values.var()
+    )
+    cnr = contrast / noise
+    return cnr
+
+
+def check_mask_exists(mask_dir, samples):
+    for i in range(len(samples["rel_path"])):
+        path = os.path.join(mask_dir, samples["rel_path"][i] + ".pt")
+        if not os.path.exists(path):
+            return False
+    return True
+
+
+def samples_to_path(mask_dir, samples, j):
+    sample_path = samples["rel_path"][j]
+    label = samples["finding_labels"][j]
+    impr = samples["impression"][j].replace(" ", "_")
+    path = os.path.join(mask_dir, sample_path + label + impr) + ".pt"
+    logger.info(f"StoPath: {path}")
+    return path
+
+
+def samples_to_path_multiquery(mask_dir, samples, j, query=""):
+    sample_path = samples["rel_path"][j]
+    label = samples["finding_labels"][j]
+    path = os.path.join(mask_dir, sample_path + label + query) + ".pt"
+    logger.info(f"StoPath: {path}")
+    return path
--- utils.py
+++ utils.py
@@ -13,7 +13,6 @@ from log import logger
 from einops import rearrange
 from scipy import ndimage
 import torchvision
-import matplotlib.pyplot as plt
 
 
 def get_compute_mask_args():
@@ -318,6 +317,64 @@ def save_copy_checkpoint(src_path, tgt_path, log_logdir=None, log_wandb=None):
         with open(os.path.join(base_path, "." + extension + ".log"), "w", encoding="utf-8") as fp:
             fp.write(f"{tgt_path} comes from {log_logdir}\n")
             fp.write(f"wandb:{log_wandb}\n")
+
+
+def update_matplotlib_font(fontsize=11, fontsize_ticks=8, tex=True):
+    import matplotlib.pyplot as plt
+    tex_fonts = {
+        # Use LaTeX to write all text
+        "text.usetex": tex,
+        "font.family": "serif",
+        # Use 11pt font in plots, to match 11pt font in document
+        "axes.labelsize": fontsize,
+        "font.size": fontsize,
+        # Make the legend/label fonts a little smaller
+        "legend.fontsize": fontsize_ticks,
+        "xtick.labelsize": fontsize_ticks,
+        "ytick.labelsize": fontsize_ticks
+    }
+    plt.rcParams.update(tex_fonts)
+
+
+def set_size(width, fraction=1, subplots=(1, 1)):
+    """Set figure dimensions to avoid scaling in LaTeX.
+
+    Parameters
+    ----------
+    width: float or string
+            Document width in points, or string of predined document type
+    fraction: float, optional
+            Fraction of the width which you wish the figure to occupy
+    subplots: array-like, optional
+            The number of rows and columns of subplots.
+    Returns
+    -------
+    fig_dim: tuple
+            Dimensions of figure in inches
+    """
+    if width == "MICCAI":
+        width_pt = 347.12354
+    else:
+        width_pt = width
+
+    # Width of figure (in pts)
+    fig_width_pt = width_pt * fraction
+    # Convert from pt to inches
+    inches_per_pt = 1 / 72.27
+
+    # Golden ratio to set aesthetic figure height
+    # https://disq.us/p/2940ij3
+    golden_ratio = (5**.5 - 1) / 2
+
+    # Figure width in inches
+    fig_width_in = fig_width_pt * inches_per_pt
+    # Figure height in inches
+    fig_height_in = fig_width_in * golden_ratio * (subplots[0] / subplots[1])
+
+    return (fig_width_in, fig_height_in)
+
+
+
 
 class AttributeDict(dict):
     def __getattr__(self, name):
